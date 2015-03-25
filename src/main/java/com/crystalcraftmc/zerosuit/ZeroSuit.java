2179a8b3a588d86b18d211acc9881d8064fd3631
Alex <www.woodrow73@gmail.com>
2015-03-24 21:39:03 -0400

    Update ZeroSuit.java

--- src/main/java/com/crystalcraftmc/zerosuit/ZeroSuit.java
+++ src/main/java/com/crystalcraftmc/zerosuit/ZeroSuit.java
@@ -1,21 +1,18 @@
-/*
- * Copyright 2015 CrystalCraftMC
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
+package com.doctorwood73.zerosuit.main;
 
-package com.crystalcraftmc.zerosuit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
+import javax.swing.Timer;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
@@ -27,15 +24,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.*;
-import java.util.ArrayList;
+import org.bukkit.util.Vector;
 
 /**Main class:
  * This program will automatically enable fly-mode when someone enters an area
@@ -403,7 +397,7 @@ public class ZeroSuit extends JavaPlugin implements Listener {
 				if(!zs.get(i).isOnline()) {
 					zs.get(i).setFlying(false);
 					zs.get(i).setAllowFlight(false);
-					zs.remove(i);
+					zs.remove(zs.get(i));
 					i--;
 					continue;
 				}
