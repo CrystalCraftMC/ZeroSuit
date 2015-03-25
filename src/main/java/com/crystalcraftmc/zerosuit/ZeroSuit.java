37dbe7e2eb67134d5075258a869552a7f35c6d0b
Alex <www.woodrow73@gmail.com>
2015-03-25 00:43:04 -0400

    fix small String literal help message

--- src/main/java/com/crystalcraftmc/zerosuit/ZeroSuit.java
+++ src/main/java/com/crystalcraftmc/zerosuit/ZeroSuit.java
@@ -103,7 +103,7 @@ public class ZeroSuit extends JavaPlugin implements Listener {
 								if(zeroArea.get(i).getID().equalsIgnoreCase(args[7])) {
 									p.sendMessage(ChatColor.RED + "Error; a zero-suit area with ID: " +
 											ChatColor.GOLD + args[7] + ChatColor.RED + " already " +
-											"exists.  Do /zerogflyperms to view all current zero-suit areas.");
+											"exists.  Do /zerog to view all current zero-suit areas.");
 									return true;
 								}
 							}
